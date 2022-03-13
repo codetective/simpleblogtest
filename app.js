662a5b4a685123da64ec41f23c336df425fd7b4d
codetective <iaminiodu@gmail.com>
2022-03-13 15:06:26 +0100

    intial commit

---
+++ app.js
@@ -0,0 +1,78 @@
+let posts;
+
+function fetchPosts() {
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((response) => response.json())
+    .then((json) => {
+      posts = [...json];
+      seedDOMWithPosts(json);
+    });
+}
+fetchPosts();
+
+const body = document.querySelector("body");
+const modal = document.querySelector("#single-post");
+const backBtn = document.querySelector("#back");
+const postcontainer = document.querySelector("#post-container");
+const singlePostContainer = document.querySelector(".post");
+const commentListContainer = document.querySelector(".comment-list");
+
+backBtn.addEventListener("click", closeModalView);
+
+function seedPostItem(item, idx) {
+  return `
+    <div class="blog-post"> <h1>${item.title}</h1>  <p>  ${item.body}  </p><a class="readBtn" href='#' data-idx='${idx}' data-id='${item.id}'>Read More</a> </div>
+ `;
+}
+function seedCommentItem(name, email, body) {
+  if (name && email) {
+    return `<li class="comment"> <h5 class="commenter-nick">${name}</h5> <h4 class="commenter-mail">${email}</h4> <p>${body}</p></li>`;
+  } else {
+    return ` <li class="comment"> <h5 class="commenter-nick">${email}</h5><p>${body}</p></li>`;
+  }
+}
+
+function seedModalView(idx) {
+  let item = posts[idx];
+  let post = `
+  <div class="post">
+  <h2 class="post-title">${item.title}</h2>
+  <div class="post-body">
+   ${item.body}
+  </div>
+</div>
+  `;
+  singlePostContainer.innerHTML = post;
+}
+
+function showPostInModal(e) {
+  modal.classList.add("show");
+  body.classList.add("stop-scrolling");
+  seedModalView(e.target.dataset.idx);
+}
+
+function closeModalView(e) {
+  modal.classList.remove("show");
+  body.classList.remove("stop-scrolling");
+}
+function attachClick() {
+  if (document.body.addEventListener) {
+    document.body.addEventListener("click", clickHandler, false);
+  } else {
+    document.body.attachEvent("onclick", clickHandler); //for IE
+  }
+}
+function clickHandler(e) {
+  e = e || window.event;
+  var target = e.target || e.srcElement;
+  if (target.className.match(/readBtn/)) {
+    showPostInModal(e);
+  }
+}
+
+function seedDOMWithPosts() {
+  posts.forEach((item, idx) => {
+    postcontainer.insertAdjacentHTML("beforeend", seedPostItem(item, idx));
+  });
+  attachClick();
+}
